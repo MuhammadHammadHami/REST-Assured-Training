f0784bfaafe25de8bada92bbb03a20080c87e240
Muhammad Hammad <hammadumer007@gmail.com>
2022-09-27 13:38:45 +0500

    Working on POJO

---
+++ src/test/java/Assignment5/base5.java
@@ -0,0 +1,14 @@
+package Assignment5;
+
+import org.testng.annotations.BeforeClass;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.baseURI;
+
+public class base5 {
+    @BeforeClass
+    public static void setup() {
+        baseURI = "https://api.getpostman.com/";
+        basePath = "environments/";
+    }
+}
